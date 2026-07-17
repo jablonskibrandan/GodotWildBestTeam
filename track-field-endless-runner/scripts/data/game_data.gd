82b3fd37faee410149cff63e2db40bee9830f035
Brandan J <brandanjablonski94@gmail.com>
2026-07-17 12:30:42 -0400

    Putting values back oops

--- track-field-endless-runner/scripts/data/game_data.gd
+++ track-field-endless-runner/scripts/data/game_data.gd
@@ -25,8 +25,8 @@ var added_100m_dash_goal_time: float = 0.0
 var added_110m_hurdles_goal_time: float = 0.0
 var added_javelin_distance_goal: float = 0.0
 
-var event_distance_min: float = 20.0
-var event_distance_max: float = 50.0
+var event_distance_min: float = 100.0
+var event_distance_max: float = 300.0
 
 # Change this if we want the exponential speed to be
 # harsher or mor subtle
